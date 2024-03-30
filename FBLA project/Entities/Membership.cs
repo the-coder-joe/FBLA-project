fa32dfb1c0762d7143336698977055b18daafdad
Joseph Kozicki <jkozicki5@gmail.com>
2024-03-29 20:59:07 -0500

    somechanges

---
+++ FBLA project/Entities/Membership.cs
@@ -0,0 +1,10 @@
+﻿namespace FBLA_project
+{
+    public class Membership
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string VehicleName { get; set; }
+
+    }
+}
